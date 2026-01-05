393d77c635aa21027f35145d2e1810d73f3cd9dc
diwanshu <diwanshu63019@gmail.com>
2026-01-05 23:33:18 +0530

    each number is the sum of the two numbers directly above it

--- Arrays/HARD/01.Pascal_s_Triangle.cpp
+++ Arrays/HARD/01.Pascal_s_Triangle.cpp
@@ -7,6 +7,9 @@ https://leetcode.com/problems/pascals-triangle/description/
 Given an integer numRows, return the first numRows of Pascal's triangle.
 
 In Pascal's triangle, each number is the sum of the two numbers directly above it
+
+input: 5
+output: [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
 */
 
 /*
